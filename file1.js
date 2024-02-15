7978900966bd166a4e9f7ac95ea7762afc19d8e1
Pavel Swon <pavelswon@gmail.com>
2024-02-15 13:43:17 +0200

    Homework for lesson 4

--- file1.js
+++ file1.js
@@ -1,4 +1,22 @@
-let wallet = 500;
-let myLeftPocket = 120;
-let totalAmount = wallet + myLeftPocket;
-console.log(totalAmount);
+function fueling(carFuel = "gas", carSize = "S") {
+    const carSizes = {
+        S: "S авто",
+        M: "M авто",
+        L: "L авто",
+        XL: "XL авто"
+    };
+    
+    const sizeText = carSizes[carSize] || "невідомий розмір авто";
+    
+    console.log(`Заправка для ${sizeText}, тип палива "${carFuel}"`);
+}
+
+fueling();
+fueling("gas", "M");
+fueling("gas", "L");
+fueling("gas", "XL");
+fueling("disel", "S");
+fueling("disel", "M");
+fueling("disel", "L");
+fueling("disel", "XL");
+fueling("gas", " ");
